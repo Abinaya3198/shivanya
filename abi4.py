025d55b6f61634e325bd0baee548e3cf87a15328
Abinaya3198 <47170772+Abinaya3198@users.noreply.github.com>
2019-03-11 11:46:21 +0530

    Create abi4.py

---
+++ abi4.py
@@ -0,0 +1,7 @@
+al = input("enter the character : ")
+if((al >= 'a' and al <= 'z') or (al >= 'A' and al <= 'Z')): 
+print("The Given Character ", ch, "is an Alphabet") 
+elif(al == '?'):
+print("no")
+else:
+print("not an alphabet")
