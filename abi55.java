7d2bff5c3c77eb55b0ee0efeb5831b2a12b3d2ca
Abinaya3198 <47170772+Abinaya3198@users.noreply.github.com>
2019-03-11 12:21:15 +0530

    Update and rename abi5.java to abi55.java

---
+++ abi55.java
@@ -0,0 +1,17 @@
+public static abi55
+{
+public static void main(String args[])
+{
+int n1,n2,n3;
+System.out.println("Enter n1,n2 and n3:");
+if(n1>n2)&&(n1>n3)
+System.out.println("n1 is larger")
+else if(n2>n3)&&(n2>n1)
+System.out.println("n2 is larger")
+else if(n3>n1)&&(n3>n2)
+System.out.println("n3 is larger")
+else:
+print("smaller")
+}
+}
+}
